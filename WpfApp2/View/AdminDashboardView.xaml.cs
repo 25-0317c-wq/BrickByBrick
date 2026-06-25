7e419d8742b3d42b65082b305d185239614b97bb
Justin <Justin@DESKTOP-4LDUQSS>
2026-06-26 02:48:25 +0800

    Pushed from sharif pc

---
+++ WpfApp2/View/AdminDashboardView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+using BrickByBrick.ViewModels;
+
+namespace BrickByBrick.View
+{
+    /// <summary>
+    /// Interaction logic for AdminDashboardView.xaml
+    /// </summary>
+    public partial class AdminDashboardView : UserControl
+    {
+        public AdminDashboardView()
+        {
+            InitializeComponent();
+            DataContext = new AdminDashboardViewModel();
+        }
+    }
+}
